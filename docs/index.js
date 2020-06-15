717196b3dac7ac1f8eb60d7fab96b660ba63867a
Shazron Abdullah <shazron@gmail.com>
2020-06-15 13:34:30 +0800

    fix fallback

--- docs/index.js
+++ docs/index.js
@@ -1,35 +1,37 @@
-function status (text) {
+function success (text) {
   if (text) {
     document.getElementById('status').textContent = text
   }
 }
 
-function post(url, data, callback) {
+function failure () {
+  success('An error occurred.')
+  document.getElementById('fallback').style = 'display:block'
+}
+
+
+function post(url, data, success, failure) {
   var xhr = new XMLHttpRequest()
   xhr.open('POST', url, true /* async */)
   xhr.setRequestHeader('X-Requested-With', 'shazron');
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
   xhr.onreadystatechange = function () {
-    console.log('readyState', this.readyState)
-    console.log('responseText', this.responseText)
     if (this.readyState == 4 /* DONE */) {
-      callback && callback(this.responseText)
+      success && success(this.responseText)
     }
   }
+  xhr.onerror = failure
   xhr.send(data)
 }
 
 setTimeout(function(){
   var url = 'http://localhost:8000/'
   console.log('Contacting ' + url)
-  post(url, "this is some data", status)
-  setTimeout(function(){
-    document.getElementById('fallback').style = 'display:block'
-  }, 2000)
+  post(url, "this is some data", success, failure)
 }, 500)
 
 function doGet() {
   var url = 'http://localhost:8000/get-test?foo=bar'
-  console.log('Contacting via get: ' + url)
+  console.log('Contacting via GET: ' + url)
   window.location = url
 }
